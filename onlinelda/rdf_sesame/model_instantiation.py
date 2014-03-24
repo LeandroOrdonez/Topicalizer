595f5fe5e15d4183a6537103f303cdf9b06f0918
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2014-03-24 12:04:09 -0500

    Updated model_instantiation.py and web_api_model.rdf: the namespace (www.example.org) has been changed (www.topicalizer.org)

--- onlinelda/rdf_sesame/model_instantiation.py
+++ onlinelda/rdf_sesame/model_instantiation.py
@@ -17,34 +17,34 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 def new_term(term_id, term):
-    return """<Term rdf:about="http://www.example.org/terms/%s">
+    return """<Term rdf:about="http://www.topicalizer.org/terms/%s">
     <has_content rdf:datatype="http://www.w3.org/2001/XMLSchema#string">%s</has_content>
     </Term>\n""" % (term_id, term)
 
 def new_membership_relation(membership_relation_id, membership_probability, category_value):
-    return """<Membership_Relation rdf:about="http://www.example.org/membership_relations/%s">
+    return """<Membership_Relation rdf:about="http://www.topicalizer.org/membership_relations/%s">
     <membership_probability rdf:datatype="http://www.w3.org/2001/XMLSchema#double">%.4f</membership_probability>
-    <category_value rdf:resource="http://www.example.org/categories/%s"/>
+    <category_value rdf:resource="http://www.topicalizer.org/categories/%s"/>
     </Membership_Relation>\n""" % (membership_relation_id, membership_probability, category_value)
 
 def new_operation(operation_id, operation_name, service_uri, membership_relations):
-    memberships = ''.join(['<is_member_of rdf:resource="http://www.example.org/membership_relations/%s"/>\n' \
+    memberships = ''.join(['<is_member_of rdf:resource="http://www.topicalizer.org/membership_relations/%s"/>\n' \
 % membership for membership in membership_relations])
-    return """<Operation rdf:about="http://www.example.org/operations/%s">
+    return """<Operation rdf:about="http://www.topicalizer.org/operations/%s">
     <has_name rdf:datatype="http://www.w3.org/2001/XMLSchema#string">%s</has_name>
     <has_service_uri rdf:datatype="http://www.w3.org/2001/XMLSchema#anyURI">%s</has_service_uri>
     %s  
     </Operation>\n""" % (operation_id, operation_name, service_uri, memberships)
 
 def new_term_relation(term_relation_id, term_probability, term_value):
-    return """<Term_Relation rdf:about="http://www.example.org/term_relations/%s">
+    return """<Term_Relation rdf:about="http://www.topicalizer.org/term_relations/%s">
     <term_probability rdf:datatype="http://www.w3.org/2001/XMLSchema#double">%.4f</term_probability>
-    <term_value rdf:resource="http://www.example.org/terms/%s"/>
+    <term_value rdf:resource="http://www.topicalizer.org/terms/%s"/>
     </Term_Relation>\n""" % (term_relation_id, term_probability, term_value)
 
 def new_category(category_id, term_relations):
-    terms = ''.join(['<has_term rdf:resource="http://www.example.org/term_relations/%s"/>\n' \
+    terms = ''.join(['<has_term rdf:resource="http://www.topicalizer.org/term_relations/%s"/>\n' \
 % term for term in term_relations])
-    return """<Category rdf:about="http://www.example.org/categories/%s">
+    return """<Category rdf:about="http://www.topicalizer.org/categories/%s">
     %s  
     </Category>\n""" % (category_id, terms)
